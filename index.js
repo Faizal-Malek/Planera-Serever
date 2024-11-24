5418cebff51f28b0bc5f0b6b6da8b0fdb0885038
Faizal-Malek <Faizalmalek03@icloud.com>
2024-11-24 12:47:59 +0200

    changing the port number

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const app = express()
+const app = express();
 
 // database connection
-mongoose.connect(process.env.MONGO_URL, {
-  serverSelectionTimeoutMS: 3000 // Wait up to 3000 milliseconds (3 seconds)
-})
-.then(() => console.log("database connected"))
-.catch((e) => console.log("database connection failed:", e))
+mongoose
+  .connect(process.env.MONGO_URL, {
+    serverSelectionTimeoutMS: 3000, // Wait up to 3000 milliseconds (3 seconds)
+  })
+  .then(() => console.log("database connected"))
+  .catch((e) => console.log("database connection failed:", e));
 
 // middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', require('./routes/authRoutes'))
+app.use("/", require("./routes/authRoutes"));
 
-const port = 8000
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+const port = 10000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
